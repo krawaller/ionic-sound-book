c43ad19a6712c26392fe2a496f26e44d0fa50057
Jacob Waller <jacob@krawaller.se>
2014-08-20 10:01:00 +0200

    Cleanup game.js

--- www/js/game.js
+++ www/js/game.js
@@ -10,12 +10,6 @@ function create() {
 
   game.stage.backgroundColor = '#ffffff';
 
-  //  Enable p2 physics
-  game.physics.startSystem(Phaser.Physics.P2JS);
-
-  game.physics.p2.gravity.y = 982;
-  game.physics.p2.defaultRestitution = 0.8;
-
   emitter = game.add.emitter(0, 0, 100);
 
   emitter.makeParticles('stars', [0, 1, 2]);
@@ -23,12 +17,6 @@ function create() {
 
   emitter.setYSpeed(-400, 400);
   emitter.setXSpeed(-400, 400);
-
-  game.input.onDown.add(onDown, this);
-
-  for (var i = 0; i < 4; i++) {
-    game.input.addPointer();
-  }
 
 }
 
@@ -43,17 +31,6 @@ ionic.on('tap', function(event) {
   //  The final parameter (10) is how many particles will be emitted in this single burst
   emitter.start(true, 1000, null, 5);
 }, document.body)
-
-function onDown(pointer) {
-  emitter.x = pointer.x;
-  emitter.y = pointer.y
-
-  //  The first parameter sets the effect to "explode" which means all particles are emitted at once
-  //  The second gives each particle a 2000ms lifespan
-  //  The third is ignored when using burst/explode mode
-  //  The final parameter (10) is how many particles will be emitted in this single burst
-  emitter.start(true, 1000, null, 5);
-}
 
 function update() {
 
